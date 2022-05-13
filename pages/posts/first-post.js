d63d1447f4832d5245d3407f81073a394664dab3
KimKyungOh <ruddhrhdwn@naver.com>
2022-05-13 11:21:01 +0900

    second commit

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,7 +3,13 @@ import Link from 'next/link';
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
+      <h1> My name: 김경오 </h1>
+      <h1> My hobby: Figure skating </h1>
+      <h1> My age: 23 </h1>
+      <h1> My MBTI: ENFJ </h1>
+      <h1> My favorite food: Chicken </h1>
+      <h1> My favorite animal: Puppy </h1>
+      <h1> Welcome to my page! </h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
